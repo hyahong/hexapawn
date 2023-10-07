087c37a2566a075628a06f613059e0bfa93b5dba
yahong <yechan0815@naver.com>
2023-10-07 05:48:34 -0700

    fin

--- hexapawn.c
+++ hexapawn.c
@@ -7,6 +7,7 @@
 
 #define PLY_DEPTH 3
 
+/* palette */
 #define C_BLACK "\033[0;30m"
 #define C_RED "\033[0;31m"
 #define C_YELLOW "\033[0;33m"
@@ -16,20 +17,25 @@
 #define C_WHITE_BLACK "\033[40;37m"
 #define C_WHITE_BBLUE "\033[104;37m"
 
+/* board size */
 #define BOARD_ROW 3
 #define BOARD_COLUMN 3
 
+/* pieces tpye */
 #define PAWN_WHITE 0
 #define PAWN_BLACK 1
 
+/* current turn */
 #define TURN_PLAYER 0
 #define TURN_AI 1
 
+/* game */
 #define WIN_NONE 1
 #define WIN_DRAW 0
 #define WIN_WHITE -1
 #define WIN_BLACK -2
 
+/* conv */
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) > (b) ? (b) : (a))
 
@@ -95,12 +101,19 @@ static int debug_beta = 0;
 /*
  * AI-algorithm
  */
+
+/*
+ * winner () - determine if the game is over
+ *   returns: WIN_NONE, WIN_DRAW, WIN_WHITE or WIN_BLACK
+ *
+ */
 int winner (void)
 {
 	int white_win, black_win;
 	int white, black;
 	int i, j;
 
+	/* if opponents piece has reached the end */
 	white_win = 0;
 	black_win = 0;
 	for (i = 1; i < BOARD_COLUMN + 1; i++)
@@ -117,13 +130,16 @@ int winner (void)
 			white_win = 1;
 		}
 	}
+	/* win condition */
 	if (white_win && black_win)
+		/* this condition can be only satisfied in the initial state */
 		return WIN_DRAW;
 	else if (white_win)
 		return WIN_WHITE;
 	else if (black_win)
 		return WIN_BLACK;
 
+	/* piece count */
 	white = 0;
 	black = 0;
 	for (i = 1; i < BOARD_ROW + 1; i++)
@@ -136,18 +152,25 @@ int winner (void)
 				black++;
 		}
 	}
+	/* if there is no pieces */
+	/* this condition can be satisfied only in initial state */
 	if (white == 0 && black == 0)
 		return WIN_DRAW;
 	/* black win */
 	if (white == 0)
 		return WIN_BLACK;
-	/* black win */
+	/* white win */
 	if (black == 0)
 		return WIN_WHITE;
 
 	return WIN_NONE;
 }
 
+/*
+ * evaluate_state () - evaluate the state
+ *   returns: return value during the game is -27 to 27, and -100 or 100
+ *			  when the game is over
+ */
 int evaluate_state (void)
 {
 	int w, b;
@@ -185,6 +208,7 @@ int evaluate_state (void)
             }
         }
     }
+	/* exceptional condition */
 	if (w == 0 && b == 0)
 		return 0;
 	else if (w == 0)
@@ -196,7 +220,18 @@ int evaluate_state (void)
 }
 
 /* alpha beta pruning */
-/* this function will envelop the logic ordering by decreasing evaluate value */
+/* search the posibilities in order to move pawn or remove the oppenets pawn */
+/* this is because there is no evaluation logic that considers 'depth' */
+
+/*
+ * hexapawn_alpha () - alpha tree of minimax algorithm
+ *   returns: maximum evaluation value
+ *   depth (in): ply-depth
+ *   alpha (in): alpha of alpha-beta pruning
+ *   beta (in): beta of alpha-beta pruning
+ *
+ * NOTE: this function will envelop the logic ordering by decreasing evaluate value
+ */
 int hexapawn_alpha (int depth, int alpha, int beta)
 {
     coordinate_t move[4] = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 } };
@@ -224,14 +259,15 @@ int hexapawn_alpha (int depth, int alpha, int beta)
         {
             if (state[i][j] == 'W')
             {
+				/* posibilities of movement */
                 for (p = 0; p < 4; p++)
                 {
                     row = i + move[p].row;
                     column = j + move[p].column;
                     
+					/* can move to those coordinates */
                     if (state[row][column] == '.')
                     {
-                        //printf ("%d %d\n", row, column);
                         state[i][j] = '.';
                         state[row][column] = 'W';
 
@@ -271,11 +307,13 @@ int hexapawn_alpha (int depth, int alpha, int beta)
         {
             if (state[i][j] == 'W')
             {
+				/* posibilities of removal */
                 for (p = 0; p < 2; p++)
                 {
                     row = i + remove[p].row;
                     column = j + remove[p].column;
 
+					/* if there is opponents pawn */
                     if (state[row][column] == 'B')
                     {
                         state[i][j] = '.';
@@ -311,6 +349,15 @@ int hexapawn_alpha (int depth, int alpha, int beta)
     return evaluation;
 }
 
+/*
+ * hexapawn_beta () - beta tree of minimax algorithm
+ *   returns: minimum evaluation value
+ *   depth (in): ply-depth
+ *   alpha (in): alpha of alpha-beta pruning
+ *   beta (in): beta of alpha-beta pruning
+ *
+ * NOTE: this function will envelop the logic ordering by decreasing evaluate value
+ */
 int hexapawn_beta (int depth, int alpha, int beta)
 {
     coordinate_t move[4] = { { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 0 } };
@@ -429,15 +476,25 @@ int hexapawn_beta (int depth, int alpha, int beta)
 /*
  * interaction
  */
+
+/*
+ * coordinate_validation () - verify that the new coordinates is valid
+ *   returns: 1 (valid) or 0 (invalid) 
+ *   pawn (in): original coordinates
+ *   new (in): target coordinates
+ */
 int coordinate_validation (coordinate_t *pawn, coordinate_t *new)
 {
+	/* removal offset */
 	coordinate_t white_remove[2] = { { 1, -1 }, { 1, 1 } };
 	coordinate_t black_remove[2] = { { -1, -1 }, { -1, 1 } };
-	coordinate_t (*remove)[2];
+	coordinate_t /* depending on a pawn color */ (*remove)[2];
+	/* movement offset */
     coordinate_t move[4] = { { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 0 } };
 	int row, column;
 	int p;
 
+	/* can move to those coordinates ? */
 	for (p = 0; p < 4; p++)
 	{
 		row = pawn->row + move[p].row;
@@ -446,6 +503,7 @@ int coordinate_validation (coordinate_t *pawn, coordinate_t *new)
 		if (row == new->row && column == new->column && state[row][column] == '.')
 			return 1;
 	}
+	/* does the opponents pawn exist at the new coordinates */
 	remove = (player_pawn == PAWN_WHITE ? &white_remove : &black_remove);
 	for (p = 0; p < 2; p++)
 	{
@@ -457,9 +515,13 @@ int coordinate_validation (coordinate_t *pawn, coordinate_t *new)
 			return 1;
 	}
 
+	/* invalid coordinates */
 	return 0;
 }
 
+/*
+ * print_state () - print the board
+ */
 void print_state (void)
 {
 	char *color;
@@ -471,8 +533,10 @@ void print_state (void)
         for (j = 1; j < BOARD_COLUMN + 1; j++)
         {
 			color = C_WHITE;
+			/* position in use */
 			if (COMPARE_POSITION (selected_pawn, i, j))
 				color = C_RED;
+			/* previous moved positions */
 			if (COMPARE_POSITION (moved_position, i, j) || COMPARE_POSITION (origin_position, i, j))
 				color = C_WHITE_BBLUE;
 			printf ("%s", color);
@@ -482,6 +546,9 @@ void print_state (void)
     }
 }
 
+/*
+ * input_state () - read the state file
+ */
 void input_state (void)
 {
 #define NUMERIC_STATE_COLOR(x) ((x) ? ((x) == 1 ? 'W' : 'B') : '.')
@@ -505,6 +572,7 @@ void input_state (void)
 			return ;
 
 		stream = fopen (answer, "r");
+		/* is this file valid ? */
 		if (stream)
 			break;
     }
@@ -512,17 +580,22 @@ void input_state (void)
 	
 	for (i = 1; i <= 3; i++)
 	{
+		/* convert */
 		fscanf (stream, "%d %d %d", &c[0], &c[1], &c[2]);
 		state[i][1] = NUMERIC_STATE_COLOR (c[0]);
 		state[i][2] = NUMERIC_STATE_COLOR (c[1]);
 		state[i][3] = NUMERIC_STATE_COLOR (c[2]);
 	}
 
+	/* close stream */
 	fclose (stream);
 
 #undef NUMERIC_STATE_COLOR
 }
 
+/*
+ * input_setting () - enter game setting
+ */
 void input_setting (void)
 {
     char answer[128] = { 0, };
@@ -553,6 +626,10 @@ void input_setting (void)
     turn = strcmp (answer, "player") ? TURN_AI : TURN_PLAYER;
 }
 
+/*
+ * print_evalbar () - draw the color bar
+ *   grid (in): white bar size and black bar size
+ */
 void print_evalbar (int *grid)
 {
 	int i;
@@ -566,7 +643,11 @@ void print_evalbar (int *grid)
 	printf ("\n" C_WHITE);
 }
 
-void print_header (int turn)
+/*
+ * print_header () - print the header
+ *   bturn (in): whether to print the turn message
+ */
+void print_header (int bturn)
 {
 	/* 0: white, 1: black */
 	int grid[2];
@@ -595,10 +676,17 @@ void print_header (int turn)
 	printf ("EVALUATION\n");
 	print_evalbar (grid);
 
-	if (turn)
+	if (bturn)
 		printf ("%s TURN\n\n", turn == TURN_PLAYER ? "PLAYER" : "AI");
 }
 
+/*
+ * TURN handling
+ */
+
+/*
+ * handle_player () - handle the user turn 
+ */
 void handle_player (void)
 {
 	coordinate_t pawn;
@@ -622,6 +710,7 @@ void handle_player (void)
 			(pawn.column >= 1 && pawn.column <= BOARD_COLUMN) &&
 			state[pawn.row][pawn.column] == (player_pawn == PAWN_WHITE ? 'W' : 'B')));
 	
+	/* mark the selected pawn */
 	COPY_POSITION (selected_pawn, pawn);
 
 	system ("clear");
@@ -645,7 +734,9 @@ void handle_player (void)
 			(new.column >= 1 && new.column <= BOARD_COLUMN) &&
 			coordinate_validation (&pawn, &new)));
 				
+	/* clear */
 	SET_POSITION (selected_pawn, -1, -1);
+	/* mark the movement */
 	COPY_POSITION (origin_position, pawn);
 	COPY_POSITION (moved_position, new);
 
@@ -653,10 +744,14 @@ void handle_player (void)
 	printf ("(%s) $> move (%d, %d) -> (%d, %d)\n", player_pawn == PAWN_WHITE ? "white" : "black",
 			pawn.row, pawn.column, new.row, new.column);
 
+	/* actual movement */
 	state[pawn.row][pawn.column] = '.';
 	state[new.row][new.column] = (player_pawn == PAWN_WHITE ? 'W' : 'B');
 }
 
+/*
+ * handle_AI () - handle the ai turn 
+ */
 void handle_AI (void)
 {
 	int evaluation;
@@ -670,10 +765,10 @@ void handle_AI (void)
 	else
 		evaluation = hexapawn_alpha (PLY_DEPTH, -10000, 10000);
 
-	printf ("(%s) $> move (%d, %d) -> (%d, %d)\n", player_pawn == PAWN_WHITE ? "black" : "white",
+	printf ("(%s) $> move (%d, %d) -> (%d, %d)\n\n", player_pawn == PAWN_WHITE ? "black" : "white",
 			origin_position.row, origin_position.column, moved_position.row, moved_position.column);
-	printf ("alpha: %d beta: %d total: %d\nsearch evaluation: %d\n",
-			debug_alpha, debug_beta, debug_alpha + debug_beta, evaluation);
+	printf ("alpha: %d beta: %d total: %d\n",
+			debug_alpha, debug_beta, debug_alpha + debug_beta);
 
 	/* reflect search results */
 	memcpy (state, next_state, (BOARD_ROW + 2) * (BOARD_COLUMN + 2));
@@ -696,8 +791,8 @@ int main (void)
 	input_state ();
 
 	system ("clear");
-	printf ("\n");
 	print_header (0);
+	printf ("\n");
     /* display the initial state */
     print_state ();
     /* get a information about player */
@@ -728,6 +823,7 @@ int main (void)
 		}
     }
 	
+	/* game is over */
 	print_header (0);
 	if (win == WIN_DRAW)
 		printf ("DRAW! ");
